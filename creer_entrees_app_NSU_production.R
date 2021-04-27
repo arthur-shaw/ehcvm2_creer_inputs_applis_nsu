0f419a3e41ede2a99717b0f06841f1afcf038803
Arthur Shaw <47256431+arthur-shaw@users.noreply.github.com>
2021-04-27 17:40:48 -0400

    Tableau de référence culture-unité-état
    Pour ce faire:
    
    - Anticiper de nouveaux onglets
    - Valider le fichier Excel avec ces infos
    - Ecrire un fichier tab avec les données requises
    
    Par ailleurs, réorganiser le contenu pour mettre toutes les étapes du même type ensemble: validation, manipulation, et création, respectivement

--- creer_entrees_app_NSU_production.R
+++ creer_entrees_app_NSU_production.R
@@ -10,17 +10,13 @@ proj_dir <- "C:/votre/chemin/"
 entree_dir <- paste0(proj_dir, "entree/")
 sortie_dir <- paste0(proj_dir, "sortie/")
 nom_fichier <- "Qx_NSU_UEMOA_EHCVM2.xlsx"
+nom_tableau <- "tableau_culture_unité_état.xlsx"
 nom_onglet_consommation <- "S1_Releves_Consommation"
 nom_onglet_production   <- "S2_Releves_Production"
 nom_onglet_unites       <- "Unités"
-
-# -----------------------------------------------------------------------------
-# Codes de produit, d'unité, et de taille attendus
-# -----------------------------------------------------------------------------
-
-# produits agricoles
-codes_produits_ag <- c(1:55)
-codes_unites_ag <- c(1:10) # TODO: Identifier l'univers de codes
+nom_onglet_cultures     <- "Cultures"
+nom_onglet_unites_prod  <- "Unités de production"
+nom_onglet_etats_prod   <- "Etats de production"
 
 # =============================================================================
 # Charger les packages requis
@@ -51,6 +47,10 @@ lapply(packagesNeeded, library, character.only = TRUE)
 # =============================================================================
 # Confirmer les inputs
 # =============================================================================
+
+# -----------------------------------------------------------------------------
+# Questionnaire NSU
+# -----------------------------------------------------------------------------
 
 # `proj_dir` existe
 assertthat::assert_that(
@@ -95,16 +95,11 @@ assertthat::assert_that(
     )
 )
 
-# =============================================================================
-# Ingérer et préparer les info de différents onglets
-# =============================================================================
-
-# -----------------------------------------------------------------------------
-# Vérifier les onglets avant de tenter d'ingérer des données
-# -----------------------------------------------------------------------------
-
 # confirmer que le fichier Excel contient les onglets attendus
-onglets_attendus <- c(nom_onglet_consommation, nom_onglet_production, nom_onglet_unites)
+onglets_attendus <- c(
+    nom_onglet_consommation, nom_onglet_production, 
+    nom_onglet_cultures, nom_onglet_unites, nom_onglet_unites_prod, nom_onglet_etats_prod
+)
 onglets_retrouves <- readxl::excel_sheets(path = paste0(entree_dir, nom_fichier))
 assert_that(
     all(onglets_attendus %in% onglets_retrouves), 
@@ -113,6 +108,61 @@ assert_that(
         Onglets attendus: {paste(onglets_attendus, collapse = ', ')}
         Onglets retrouvés: {paste(onglets_retrouves, collapse = ', ')}")
 )
+
+
+# -----------------------------------------------------------------------------
+# Tableau culture-unité-état
+# -----------------------------------------------------------------------------
+
+# `nom_fichier` existe dans `proj_dir`
+assertthat::assert_that(
+    file.exists(paste0(entree_dir, nom_tableau)),
+    msg = glue::glue(
+        "Le questionnaire dans `nom_tableau` n'existe pas dans le répertoire `proj_dir`.",
+        "Répertoire: {proj_dir}",
+        "Fichier indiqué: {nom_tableau}",
+        .sep = "\n"
+    )
+)
+
+# fichier `nom_fichier` n'est pas un fichier Excel
+assertthat::assert_that(
+    readxl::excel_format(path = paste0(entree_dir, nom_tableau)) %in% c("xlsx", "xls"),
+    msg = glue::glue(
+        "Le fichier désigné comme questionnaire n'est pas un fichier Excel",
+        "Veuillez saisir un fichier Excel dans `nom_tableau`",
+        .sep = "\n"
+    )
+)
+
+# =============================================================================
+# Ingérer et préparer les info de différents onglets du qnr NSU
+# =============================================================================
+
+# -----------------------------------------------------------------------------
+# Ingérer les onglets de contrôle
+# -----------------------------------------------------------------------------
+
+# cultures
+onglet_cultures <- onglet_unites <- readxl::read_excel(
+    path = paste0(entree_dir, nom_fichier),
+    sheet = nom_onglet_cultures
+)
+codes_cultures <- pull(onglet_cultures, 2L)
+
+# unités
+onglet_unites <- readxl::read_excel(
+    path = paste0(entree_dir, nom_fichier),
+    sheet = nom_onglet_unites_prod
+)
+codes_unites <- pull(onglet_unites, 2L)
+
+# états
+onglet_etats <- readxl::read_excel(
+    path = paste0(entree_dir, nom_fichier),
+    sheet = nom_onglet_etats_prod
+)
+codes_etats <- pull(onglet_etats, 2L)
 
 # -----------------------------------------------------------------------------
 # Ingérer les produits de production
@@ -136,7 +186,7 @@ nsu_production_df <- readxl::read_excel(
     filter(!(is.na(produit) & is.na(unite)))
 
 # confirmer qu'il existe 13 colonnes
-nsu_consommation_ncol <- ncol(nsu_consommation_df)
+nsu_consommation_ncol <- ncol(nsu_production_df)
 assert_that(
     nsu_consommation_ncol == 13,
     msg = glue::glue("//
@@ -174,9 +224,9 @@ nsu_production <- nsu_production_df %>%
         unite = haven::labelled(unite, labels = nsu_unites)      
     )
 
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# -----------------------------------------------------------------------------
 # Vérifier le contenu des colonnes; avertir en cas de problème
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# -----------------------------------------------------------------------------
 
 # créer un agent de vérification et intéroger la base
 agent_conso <- pointblank::create_agent(
@@ -188,16 +238,15 @@ agent_conso <- pointblank::create_agent(
         label = "Tous les produits ont un code valide", 
         step_id = 1, 
         columns = vars(produit), 
-        set = codes_produits
+        set = codes_cultures
     ) %>%
     pointblank::col_vals_in_set(
         label = "Toutes les unités ont un code valide",
         step = 2,
         columns = vars(unite),
-        set = nsu_unites
+        set = codes_unites
     ) %>%
     pointblank::interrogate()
-
 
 # émettre des erreurs si des problèmes ont été repérés
 if(pointblank::all_passed(agent = agent_conso) == FALSE) {
@@ -243,8 +292,113 @@ if(pointblank::all_passed(agent = agent_conso) == FALSE) {
 }
 
 # =============================================================================
-# Créer un document HTML qui capte les unités et tailles pour chaque produit
+# Ingérer et préparer le tableau culture-unité-état
 # =============================================================================
+
+# ingérer la base du fichier Excel
+tableau_df <- readxl::read_excel(paste0(entree_dir, nom_tableau)) %>%
+    # retenir les colonnes requises, listées ci-haut
+    select(all_of(cols_necessaires)) %>%
+    # retenir les lignes avec un contenu entièrement non-vide
+    filter(if_all(
+        .cols = everything(),
+        .fns = ~ !is.na(.x)
+    )) %>%
+    # renommer les colonnes: laisser tomber le suffixe
+    rename_with(
+        .cols = everything(),
+        .fn = ~ stringr::str_replace(.x, "_code", "")
+    )
+
+# créer un agent de vérification et intéroger la base
+agent_tableau <- pointblank::create_agent(
+        tbl = tableau_df,
+        label = "Vérifier le contenu du tableau Excel",
+        actions = pointblank::action_levels(stop_at = 1)
+    ) %>%
+    pointblank::col_vals_in_set(
+        label = "Toutes les cultures ont un code valide", 
+        step_id = 1, 
+        columns = vars(code_culture), 
+        set = codes_cultures
+    ) %>%
+    pointblank::col_vals_in_set(
+        label = "Toutes les unités ont un code valide",
+        step = 2,
+        columns = vars(code_unite),
+        set = codes_unites
+    ) %>%
+    pointblank::col_vals_in_set(
+        label = "Tous les états ont un code valide",
+        step = 3,
+        columns = vars(code_etat),
+        set = codes_etats
+    ) %>%
+    pointblank::interrogate()
+
+# émettre des erreurs si des problèmes ont été repérés
+if(pointblank::all_passed(agent = agent_tableau) == FALSE) {
+
+    # get agent's x-list
+    tableau_xlist <- pointblank::get_agent_x_list(agent = agent_tableau)
+    
+    # determine which didn't pass
+    which_steps_failed <- which(tableau_xlist$n_failed > 0)
+
+    # Tous les produits ont un code valide
+    if (1 %in% which_steps_failed) {
+
+        fails_step1 <- get_data_extracts(agent = agent_tableau, i = 1)
+        print(fails_step1)
+        assert_that(
+            1 == 0,
+            msg = glue::glue(
+                "Certaines cultures n'ont pas de codes valide.
+                Veuillez saisir `View(fails_step1)` pour visualiser les cultures problématiques"
+            )
+        )
+
+    }
+
+    # Toutes les unités ont un code valide
+    if (2 %in% which_steps_failed) {
+
+        fails_step2 <- get_data_extracts(agent = agent_tableau, i = 2)
+        print(fails_step2)
+        assert_that(
+            1 == 0,
+            msg = glue::glue(
+                "Certaines unités n'ont pas de codes valide.
+                Veuillez saisir `View(fails_step2)` pour visualiser les codes d'unité problématiques."
+            )
+        )
+
+    }
+
+    # Tous les états ont un code valide
+    if (3 %in% which_steps_failed) {
+
+        fails_step2 <- get_data_extracts(agent = agent_tableau, i = 3)
+        print(fails_step3)
+        assert_that(
+            1 == 0,
+            msg = glue::glue(
+                "Certains états n'ont pas de codes valide.
+                Veuillez saisir `View(fails_step3)` pour visualiser les codes d'unité problématiques."
+            )
+        )
+
+    }
+
+}
+
+# =============================================================================
+# Créer les sorties
+# =============================================================================
+
+# -----------------------------------------------------------------------------
+# Créer un document HTML qui capte les unités et tailles pour chaque produit
+# -----------------------------------------------------------------------------
 
 rmarkdown::render(
     input = paste0(proj_dir, "nsu_codes_appli_prod.Rmd"),
@@ -252,3 +406,14 @@ rmarkdown::render(
     output_file = "nsu_codes_appli_prod.html",
     encoding = "UTF-8"
 )
+
+# -----------------------------------------------------------------------------
+# Créer un tableau de référence sous format délimité par tab
+# -----------------------------------------------------------------------------
+
+readr::write_tsv(
+    tableau_df, 
+    file = paste0(sortie_dir, "tableau_culture_unite_etat.tab")
+)
+
+
